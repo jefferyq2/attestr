bf2dff9f669defeffff0e64fe2ed5596c237cea6
mrjoelkamp <joel.kamp@docker.com>
2024-04-22 12:50:52 -0500

    revert: add exported constants

--- pkg/types/types.go
+++
@@ -1,7 +0,0 @@
-package types
-
-const (
-	DockerReferenceType     = "vnd.docker.reference.type"
-	DockerReferenceDigest   = "vnd.docker.reference.digest"
-	AttestationManifestType = "attestation-manifest"
-)
