6b199f027a5e9c2540b109b1704366d6b23a1ec2
James Carnegie <kipz@users.noreply.github.com>
2024-07-09 15:02:49 +0100

    Enable GCP integration test (#82)

--- pkg/signerverifier/gcp_test.go
+++ pkg/signerverifier/gcp_test.go
@@ -1,4 +1,16 @@
+//go:build e2e
+
 package signerverifier
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"testing"
+
+	"github.com/docker/attest/internal/util"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 const publicKeyPEM = `-----BEGIN PUBLIC KEY-----
 MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEuMswW3iu7PR/rWTQjlhVmUsPK7rF
@@ -8,26 +20,26 @@ k2s4SO3XbQ2GG2alm289SUUpmBAuVxvT8muYQ8HC/QzixzyTACTXsBDjQg==
 // to run locally, we need to impersonate the GCP service account
 // gcloud auth application-default login --impersonate-service-account attest-kms-test@attest-kms-test.iam.gserviceaccount.com
 
-// func TestGCPKMS_Signer(t *testing.T) {
-// 	// create a new signer
-// 	ctx := context.Background()
-// 	ref := "projects/attest-kms-test/locations/us-west1/keyRings/attest-kms-test/cryptoKeys/test-signing-key/cryptoKeyVersions/1"
-// 	signer, err := GetGCPSigner(ctx, ref)
-// 	require.NoError(t, err)
-// 	msg := []byte("hello world")
-// 	hash := util.SHA256(msg)
+func TestGCPKMS_Signer(t *testing.T) {
+	// create a new signer
+	ctx := context.Background()
+	ref := "projects/attest-kms-test/locations/us-west1/keyRings/attest-kms-test/cryptoKeys/test-signing-key/cryptoKeyVersions/1"
+	signer, err := GetGCPSigner(ctx, ref)
+	require.NoError(t, err)
+	msg := []byte("hello world")
+	hash := util.SHA256(msg)
 
-// 	// sign message digest
-// 	sig, err := signer.Sign(ctx, hash)
-// 	require.NoError(t, err)
-// 	assert.NotEmpty(t, sig)
-// 	// get Key ID from signer
-// 	keyId, err := signer.KeyID()
-// 	require.NoError(t, err)
-// 	assert.NotEmpty(t, keyId)
-// 	publicKey, err := Parse([]byte(publicKeyPEM))
-// 	require.NoError(t, err)
-// 	// verify payload ecdsa signature
-// 	ok := ecdsa.VerifyASN1(publicKey, hash, sig)
-// 	assert.True(t, ok)
-// }
+	// sign message digest
+	sig, err := signer.Sign(ctx, hash)
+	require.NoError(t, err)
+	assert.NotEmpty(t, sig)
+	// get Key ID from signer
+	keyId, err := signer.KeyID()
+	require.NoError(t, err)
+	assert.NotEmpty(t, keyId)
+	publicKey, err := Parse([]byte(publicKeyPEM))
+	require.NoError(t, err)
+	// verify payload ecdsa signature
+	ok := ecdsa.VerifyASN1(publicKey, hash, sig)
+	assert.True(t, ok)
+}
