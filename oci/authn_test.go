01a6a2ab7d47ccb5fd07d8b5b16b7001327c1f53
mrjoelkamp <joel.kamp@docker.com>
2024-10-18 09:25:31 -0500

    refactor: remove copyright year; add newline

--- oci/authn_test.go
+++ oci/authn_test.go
@@ -1,7 +1,7 @@
 //go:build e2e
 
 /*
-   Copyright 2024 Docker attest authors
+   Copyright Docker attest authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
+
 package oci_test
 
 import (
